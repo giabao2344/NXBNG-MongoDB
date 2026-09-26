1eb518e523e5a4cc38d0fe7f32e16bad677e90a7
giabao2344 <otakuohma@gmail.com>
2026-09-26 15:44:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Địa chỉ backend MongoDB API khi chạy local.
 // Khi deploy API lên hosting, đổi thành URL API thật, ví dụ:
 // window.LNN_API_BASE = 'https://api.innongnghiep.com';
-window.LNN_API_BASE = 'http://localhost:3000';
+window.LNN_API_BASE = 'https://lnn-mongodb-api.onrender.com';
